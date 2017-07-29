a41468a0189884fb4060ce777e08c601880137f8
Vincent Warmerdam <vicnent3006@gmail.com>
2017-07-29 21:25:51 +0200

    BT04 26/28 - Adding a Tick(), OUT parameters and Finding Pixel Coordinates

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -19,9 +19,70 @@ void ATankPlayerController::BeginPlay()
     }
 }
 
+// Called every frame
+void ATankPlayerController::Tick( float DeltaTime )
+{
+    Super::Tick( DeltaTime );
+    AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+    if (!GetControlledTank()) { return; }
+    
+    FVector OutHitLocation; // out param
+    if (GetSightRayHitLocation(OutHitLocation))
+    {
+        //UE_LOG(LogTemp, Warning, TEXT("LookDirection: %s"), *OutHitLocation.ToString());
+        // tell controlled tank to aim at this point
+    }
+}
 
+// get world location of linetrace though crosshair, true if it hits the landscape
+bool ATankPlayerController::GetSightRayHitLocation( FVector& OutHitLocation ) const
+{
+    // ray-cast out to reach distance
+    FHitResult HitResult;
+    FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+    GetWorld()->LineTraceSingleByObjectType(
+                                            OUT HitResult,
+                                            GetTraceLineStart(),
+                                            GetTraceLineEnd(),
+                                            FCollisionObjectQueryParams(ECollisionChannel::ECC_WorldDynamic),
+                                            TraceParams
+                                            );
+    // find crosshair position in pixel coordinates
+    int32 ViewportSizeX, ViewportSizeY;
+    GetViewportSize(ViewportSizeX, ViewportSizeY);
+    auto ScreenLocation = FVector2D((ViewportSizeX*CrossHairXLocation), (ViewportSizeY*CrossHairYLocation));
+    
+    // de-project the screen position of the crosshair to a world direction
+    // line trace along that look direction and see what we hit
+    OutHitLocation = FVector(1.0);
+    return true;
+}
+
+FVector ATankPlayerController::GetTraceLineStart() const
+{
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+                                                               OUT PlayerViewPointLocation,
+                                                               OUT PlayerViewPointRotation);
+    return PlayerViewPointLocation;
+}
+
+FVector ATankPlayerController::GetTraceLineEnd() const
+{
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+                                                               OUT PlayerViewPointLocation,
+                                                               OUT PlayerViewPointRotation);
+    return PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,10 +14,30 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
-    ATank* GetControlledTank() const;
+private:
+    // Called every frame
+    virtual void Tick( float DeltaSeconds ) override;
     
     virtual void BeginPlay() override;
-	
-	
+    
+    ATank* GetControlledTank() const;
+    
+    // Start moving the barrel of the tank so that a shot would hit where the crosshair intersects the world.
+    void AimTowardsCrosshair();
+    
+    bool GetSightRayHitLocation( FVector& OutHitLocation ) const;
+    
+    UPROPERTY(EditAnywhere)
+    float Reach = 2500.f;
+    
+    UPROPERTY(EditAnywhere)
+    float CrossHairXLocation = 0.5f;
+    
+    UPROPERTY(EditAnywhere)
+    float CrossHairYLocation = 0.33333f;
+    
+    FVector GetTraceLineStart() const;
+    
+    FVector GetTraceLineEnd() const;
+    
 };
